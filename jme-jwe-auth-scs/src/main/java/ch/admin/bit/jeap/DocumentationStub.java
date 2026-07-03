b09533a81d1b8b05853d289baaff7482940b2035
Matthias Fritschi <matthias.fritschi@bit.admin.ch>
2026-07-03 15:13:59 +0200

    JEAP-7096: attach empty javadoc jar instead of a documentation stub class

--- jme-jwe-auth-scs/src/main/java/ch/admin/bit/jeap/DocumentationStub.java
+++
@@ -1,9 +0,0 @@
-package ch.admin.bit.jeap;
-
-/**
- * Placeholder class to satisfy Javadoc generation requirements.
- * This class does not contain any logic.
- */
-public class DocumentationStub {
-    // Intentionally left blank
-}
